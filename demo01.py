5f4fc66df9f316a78e217af67c44ab21975abeed
blackyangA <931395490@qq.com>
2019-10-11 21:52:01 +0800

    WEB_01 first commit !

---
+++ demo01.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# @Time : 2019/10/11 21:47
+# @Software: PyCharm
+# @author: 洋
+# @File : demo01.py
+print("Hello World !")
---
+++ demo02.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# @Time : 2019/10/11 21:48
+# @Software: PyCharm
+# @author: 洋
+# @File : demo02.py
+print("Hi China !")
